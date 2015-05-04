04f526e36fc994e21f007445e1c2fc80035c1a5f
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-05-05 01:01:47 +0900

    ユニットテストプロジェクト 追加

---
+++ UnitTestProject/Linq/Linqx.cs
@@ -0,0 +1,170 @@
+﻿using Lavn.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.Linq
+{
+	[TestClass]
+	public class Linqx
+	{
+		[TestMethod, TestCategory("Linqx")]
+		public void CountOver_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountOver(2);
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountOver_False()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountOver(3);
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountOver_Predicate_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountOver(2, e => e.Contains("Test"));
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountOver_Predicate_False()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountOver(3, e => e.Contains("Test"));
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountUnder_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountUnder(4);
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountUnder_False()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountUnder(3);
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountUnder_Predicate_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountUnder(4, e => e.Contains("Test"));
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountUnder_Predicate_False()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountUnder(3, e => e.Contains("Test"));
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountEquals(3);
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_False1()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountEquals(2);
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_False2()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.CountEquals(4);
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_Predicate_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountEquals(3, e => e.Contains("Test"));
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_Predicate_False1()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountEquals(2, e => e.Contains("Test"));
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void CountEquals_Predicate_False2()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+				"Start_XXXX4_End",
+			}.CountEquals(4, e => e.Contains("Test"));
+			Assert.AreEqual(false, result);
+		}
+	}
+}
---
+++ UnitTestProject/Linq/Linqx_Count.cs
@@ -0,0 +1,42 @@
+﻿using Lavn.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.Linq
+{
+	[TestClass]
+	public class Linqx_Count
+	{
+		[TestMethod, TestCategory("Linqx")]
+		public void MixedResult_True()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.MixedResult(e => e.StartsWith("Start"));
+			Assert.AreEqual(true, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void MixedResult_False()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.MixedResult(e => e.EndsWith("Start"));
+			Assert.AreEqual(false, result);
+		}
+
+		[TestMethod, TestCategory("Linqx")]
+		public void MixedResult_Null()
+		{
+			var result = new[] {
+				"Start_Test1_End",
+				"Start_Test2_End",
+				"Start_Test3_End",
+			}.MixedResult(e => e.Contains("Test1"));
+			Assert.AreEqual(null, result);
+		}
+	}
+}
